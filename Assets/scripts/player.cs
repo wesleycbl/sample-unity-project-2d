fb514806a7dc47d8748e70354dad56e35a05a492
Wesley <wesleypsantos@hotmail.com>
2021-02-24 01:14:12 -0300

    autosave with format csharp

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -11,8 +11,8 @@ public class player : MonoBehaviour
     public bool grounded;
     public bool attacking;
     public int idAnimation;
-    public Transform groundCheck; 
-    public float speed; 
+    public Transform groundCheck;
+    public float speed;
     public float jumpForce;
     public bool lookLeft;
     // start
@@ -20,46 +20,58 @@ public class player : MonoBehaviour
     void Start()
     {
         _playerAnimator = GetComponent<Animator>(); // inicializando compoennte
-        _playerRb       = GetComponent<Rigidbody2D>(); // inicializando compoennte
+        _playerRb = GetComponent<Rigidbody2D>(); // inicializando compoennte
     }
     // comandos que utilizam de manuceio de Fisica, deve ser utilizado esse lifecyrcle
-    void FixedUpdate() { //taxa de atualização fixa de 0.02
+    void FixedUpdate()
+    { //taxa de atualização fixa de 0.02
         grounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
         _playerRb.velocity = new Vector2(_horizontal * speed, _playerRb.velocity.y);
-    } 
+    }
     void Update()
     {
-        _horizontal = Input.GetAxisRaw("Horizontal"); 
-        _vertical   = Input.GetAxisRaw("Vertical"); 
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
 
-        if (lookLeft && _horizontal > 0 && attacking == false) {
-            flip();
-        } else if (!lookLeft && _horizontal < 0  && attacking == false) {
+        if (lookLeft && _horizontal > 0 && attacking == false)
+        {
             flip();
         }
-        
-        if (_vertical < 0) {
+        else if (!lookLeft && _horizontal < 0 && attacking == false)
+        {
+            flip();
+        }
+
+        if (_vertical < 0)
+        {
             idAnimation = 2;
-            if (grounded) {
+            if (grounded)
+            {
                 _horizontal = 0;
             }
             _horizontal = 0;
         }
-        else if (_horizontal != 0) {
+        else if (_horizontal != 0)
+        {
             idAnimation = 1;
-        } else {
+        }
+        else
+        {
             idAnimation = 0;
         }
 
-        if(Input.GetButtonDown("Fire1") && _vertical >= 0 && attacking == false) {
+        if (Input.GetButtonDown("Fire1") && _vertical >= 0 && attacking == false)
+        {
             _playerAnimator.SetTrigger("attack");
         }
 
-        if(Input.GetButtonDown("Jump") && grounded == true && attacking == false) {
+        if (Input.GetButtonDown("Jump") && grounded == true && attacking == false)
+        {
             _playerRb.AddForce(new Vector2(0, jumpForce));
         }
 
-        if(attacking && grounded){
+        if (attacking && grounded)
+        {
             _horizontal = 0;
         }
 
@@ -68,12 +80,13 @@ public class player : MonoBehaviour
         _playerAnimator.SetFloat("speedY", _playerRb.velocity.y);
     }
 
-    void flip() {
+    void flip()
+    {
         lookLeft = !lookLeft;
 
         float x = transform.localScale.x;
         x *= -1;
-        transform.localScale 
+        transform.localScale
             = new Vector3(
                 x,
                 transform.localScale.y,
@@ -81,8 +94,10 @@ public class player : MonoBehaviour
             );
     }
 
-    void attack(int attack){
-        switch(attack) {
+    void attack(int attack)
+    {
+        switch (attack)
+        {
             case 0:
                 attacking = false;
                 break;
